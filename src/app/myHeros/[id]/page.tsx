db49d4bda567453b61edae9e6e3aeeb605a93eb7
Kento Miyamoto <miyamoto.kento491@mail.kyutech.jp>
2025-04-21 11:45:03 +0000

    レビュー修正

--- src/app/myHeros/[id]/page.tsx
+++ src/app/myHeros/[id]/page.tsx
@@ -1,16 +1,21 @@
-import Hero from './;HeroDetails';
+import Hero from "./;HeroDetails";
 
 export const metadata = {
 	title: "俺のヒーロー紹介",
 };
 
-const MemoDetail = ({params}:{params: {id: string}}) => {
-
-  return (
-    <main>
-        <Hero id={params.id}/>
-    </main>
-  );
+/**
+ * @description
+ * ヒーローの詳細を表示するコンポーネント
+ * @param {string} id - ヒーローのID
+ * @returns {JSX.Element} ヒーローの詳細を表示する要素
+ */
+const MyHeroesDetail = ({ params }: { params: { id: string } }) => {
+	return (
+		<main>
+			<Hero id={params.id} />
+		</main>
+	);
 };
 
-export default MemoDetail;
+export default MyHeroesDetail;
